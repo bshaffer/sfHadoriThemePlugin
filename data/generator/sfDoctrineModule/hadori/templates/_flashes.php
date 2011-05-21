1c7a4e31ff5a155c439dc0e4dc337a8c9040453d
Matias Alejo Roldan Zukerfeld <matiasalejoroldan@gmail.com>
2011-05-21 19:20:51 -0300

    Fixed css selector for flash messages and rollback  *class* to *id* in _flashes.php

--- data/generator/sfDoctrineModule/hadori/templates/_flashes.php
+++ data/generator/sfDoctrineModule/hadori/templates/_flashes.php
@@ -1,11 +1,11 @@
 <?php if ($sf_user->hasFlash('error')): ?>
-  <div class="error"><?php echo $sf_user->getFlash('error') ?></div>
+  <div id="error"><?php echo $sf_user->getFlash('error') ?></div>
 <?php endif; ?>
 
 <?php if ($sf_user->hasFlash('info')): ?>
-  <div class="info"><?php echo $sf_user->getFlash('info') ?></div>
+  <div id="info"><?php echo $sf_user->getFlash('info') ?></div>
 <?php endif; ?>
 
 <?php if ($sf_user->hasFlash('notice')): ?>
-  <div class="notice"><?php echo $sf_user->getFlash('notice') ?></div>
+  <div id="notice"><?php echo $sf_user->getFlash('notice') ?></div>
 <?php endif; ?>
